864d8504a99c2f3284f25f52d5ab09b1c7c841b0
mendi <mendenhall_chris1246@yahoo.com>
2023-07-29 17:25:47 -0700

    added buttons with click listeners

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+let btnOne = document.getElementById("btnOne");
+let btnTwo = document.getElementById("btnTwo");
+let btnThree = document.getElementById("btnThree");
 
+btnOne.addEventListener("click", () => {
+	console.log("Generate Die");
+});
+
+btnTwo.addEventListener("click", () => {
+	console.log("Roll Dice");
+});
+
+btnThree.addEventListener("click", () => {
+	console.log("Sum Die");
+});

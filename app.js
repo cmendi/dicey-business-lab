aa563de62fe6a9aafeae40972f724c9edb7fbd23
mendi <mendenhall_chris1246@yahoo.com>
2023-07-30 14:48:45 -0700

    finished lab

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-let btnOne = document.getElementById("btnOne");
-let btnTwo = document.getElementById("btnTwo");
-let btnThree = document.getElementById("btnThree");
+// Global variables
+const btnOne = document.getElementById("btnOne");
+const btnTwo = document.getElementById("btnTwo");
+const btnThree = document.getElementById("btnThree");
+const dieDiv = document.getElementById("dieDiv");
+let diceVal = [];
+
+class Die {
+	constructor() {
+		this.div = document.createElement("div");
+		this.divStyle();
+		this.roll();
+		diceVal.push(this);
+		dieDiv.appendChild(this.div);
+	}
+
+	// Could of just done this in my style.css file but i wanted to practice.
+	divStyle() {
+		this.div.style.height = "150px";
+		this.div.style.width = "150px";
+		this.div.style.border = "4px solid black";
+		this.div.style.borderRadius = "15px";
+		this.div.style.margin = "20px";
+		this.div.style.cursor = "pointer";
+		this.div.style.display = "flex";
+		this.div.style.alignItems = "center";
+		this.div.style.justifyContent = "center";
+		this.div.style.fontSize = "70px";
+	}
+
+	roll() {
+		this.value = Math.floor(Math.random() * 6 + 1);
+		this.div.textContent = this.value;
+	}
+}
 
 btnOne.addEventListener("click", () => {
-	console.log("Generate Die");
+	new Die();
+	// console.log("Generate Die");
 });
 
 btnTwo.addEventListener("click", () => {
-	console.log("Roll Dice");
+	for (let i = 0; i < diceVal.length; i++) {
+		diceVal[i].roll();
+	}
+	// console.log("Roll Dice");
 });
 
 btnThree.addEventListener("click", () => {
-	console.log("Sum Die");
+	let sum = 0;
+	for (let i = 0; i < diceVal.length; i++) {
+		sum += diceVal[i].value;
+	}
+	alert(`The sum of the dice is ${sum}`);
 });
